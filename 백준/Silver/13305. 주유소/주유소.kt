1898926e4557cf5b23226eec9b3cc189239b5f03
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-10-11 15:05:01 +0900

    [Silver III] Title: 주유소, Time: 604 ms, Memory: 54056 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val n = readln().toInt()
+    val edges = readln().split(" ").map { it.toInt() }
+    val prices = readln().split(" ").map { it.toInt() }.toIntArray()
+
+    val total = edges.sum()
+    var remaining = total
+
+    val result = edges
+        .mapIndexed { index, edge ->
+            val prop = prices[index] to remaining
+            remaining -= edge
+
+            prop
+        }
+        .sortedBy { it.first }
+        .sumOf { (price, distance) ->
+            val assigned = (distance - remaining).coerceAtLeast(0)
+
+            remaining += assigned
+
+            price * assigned
+        }
+
+    println(result)
+}
