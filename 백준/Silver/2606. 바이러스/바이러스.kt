1eb814bfbedc0c82334f9175374b71f6cff8cc04
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-09 05:18:18 +0900

    [Silver III] Title: 바이러스, Time: 164 ms, Memory: 24284 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.kt
@@ -0,0 +1,48 @@
+import java.util.*
+
+fun bfs(v: Int, start: Int, edgeMap: Map<Int, List<Int>>): Int {
+    val discovered = BooleanArray(v)
+    val queue: Queue<Int> = LinkedList()
+
+    discovered[start] = true
+    queue.add(start)
+
+    var nowDistance = -1
+    while (queue.isNotEmpty()) {
+        ++nowDistance
+        repeat(queue.size) {
+            val now = queue.poll()
+            val neighbors = edgeMap[now] ?: emptyList()
+            for (neighbor in neighbors) {
+                if (discovered[neighbor])
+                    continue
+
+                discovered[neighbor] = true
+                queue.add(neighbor)
+            }
+        }
+    }
+    return discovered.count { it } - 1
+}
+
+fun solve(v: Int, edges: List<Pair<Int, Int>>): Int {
+    val edgeMap = edges
+        .flatMap {
+            listOf(it, it.second to it.first)
+        }
+        .groupBy({ it.first }, { it.second })
+
+    return bfs(v + 1, 1, edgeMap)
+}
+
+fun main() {
+    val v = readln().toInt()
+    val e = readln().toInt()
+    val edges = List(e) {
+        readln().split(" ").map { it.toInt() }.let { it[0] to it[1] }
+    }
+
+    val result = solve(v, edges)
+
+    println(result)
+}
