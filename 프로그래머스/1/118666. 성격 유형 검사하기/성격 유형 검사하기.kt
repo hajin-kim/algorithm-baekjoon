b8fff9ae615f1273bc63c211982dbde78c53216b
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-24 18:28:37 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 15.68 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -0,0 +1,35 @@
+// 18:15 ~
+class Solution {
+    fun solution(survey: Array<String>, choices: IntArray): String {
+        val n = survey.size
+
+        val scoreMap = mutableMapOf<Char, Int>()
+
+        repeat(n) {
+            val (negative, positive) = survey[it].toCharArray()
+            val choice = choices[it]
+
+            if (choice > 4) {
+                scoreMap[positive] = (scoreMap[positive] ?: 0) + (choice - 4)
+            } else if (choice < 4) {
+                scoreMap[negative] = (scoreMap[negative] ?: 0) + (4 - choice)
+            }
+        }
+
+        return buildString {
+            val options = listOf(
+                listOf('R', 'T'),
+                listOf('C', 'F'),
+                listOf('J', 'M'),
+                listOf('A', 'N'),
+            )
+
+            for ((first, second) in options) {
+                if ((scoreMap[first] ?: 0) >= (scoreMap[second] ?: 0))
+                    append(first)
+                else
+                    append(second)
+            }
+        }
+    }
+}
