a2c6b12846935b55e6e5d4f011d56ce927cbe713
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-08-01 00:04:03 +0900

    [Silver I] Title: 안전 영역, Time: 484 ms, Memory: 42284 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.kt
@@ -0,0 +1,52 @@
+typealias Array2<T> = Array<Array<T>>
+
+inline fun <T, reified R> Array2<T>.map2(f: (T) -> R): Array2<R> =
+    map { it.map(f).toTypedArray() }.toTypedArray()
+
+fun Array2<Int>.isSafeFromJangma(gansuryang: Int): Array2<Boolean> =
+    map2 { it > gansuryang }
+
+fun Array2<Boolean>.markFalseRecursively(x: Int, y: Int) {
+    if (x < 0 || y < 0 || x >= size || y >= size) {
+        return
+    }
+    if (!this[y][x]) return
+
+    this[y][x] = false
+    markFalseRecursively(x = x + 1, y = y)
+    markFalseRecursively(x = x - 1, y = y)
+    markFalseRecursively(x = x, y = y + 1)
+    markFalseRecursively(x = x, y = y - 1)
+}
+
+fun Array2<Boolean>.getNumberOfSafeZones(): Int {
+    var numberOfSafeZones = 0
+
+    mapIndexed { y, xs ->
+        xs.mapIndexed { x, isSafe ->
+            if (isSafe) {
+                numberOfSafeZones++
+                markFalseRecursively(x = x, y = y)
+            }
+        }
+    }
+
+    return numberOfSafeZones
+}
+
+fun main() {
+    val size = readln().toInt()
+    val zone = (1..size).map { readln().split(" ").map { it.toInt() }.toTypedArray() }.toTypedArray()
+
+    val maxGansuryang = zone.maxOfOrNull { it.max() }!!
+
+    val gansuryangs = 0..maxGansuryang
+
+    val maxNumberOfSafeZones = gansuryangs.maxOfOrNull { gansuryang ->
+        zone
+            .isSafeFromJangma(gansuryang)
+            .getNumberOfSafeZones()
+    } ?: 0
+
+    println(maxNumberOfSafeZones)
+}
